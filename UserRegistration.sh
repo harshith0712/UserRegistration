18b84357829f3b553f8cea2c78012f89fb6fd1d8
harshith0712 <kolagatlaharshith@gmail.com>
2020-10-13 20:01:31 +0530

    usecase1

--- UserRegistration.sh
+++ UserRegistration.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
-echo "Welcome To User Registration Problem"
+echo "enter the first name"
+read firstName
+firstPattern="^[A-Z][a-z]{2,}$"
+
+if [[ $firstName =~ $firstPattern ]]
+then
+echo $firstName
+else
+echo "enter correct first name"
+fi
+
+
