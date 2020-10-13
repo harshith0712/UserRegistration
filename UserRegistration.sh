f8cd4e69b4664d3541e279b5747d79960054e54a
harshith0712 <kolagatlaharshith@gmail.com>
2020-10-13 20:22:22 +0530

    usecase4

--- UserRegistration.sh
+++ UserRegistration.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-echo "Welcome To User Registration Problem"
+echo "enter the mobile number "
+read mobileNumber
+mobilePattern="^[0-9]{2}[[:space:]][0-9]{10}$"
+	if [[ $mobileNumber =~ $mobilePattern ]]
+	then
+		echo $mobileNumber
+	else
+		echo "enter valid mobile number"
+	fi
