d1d683e9452551901917bb8c8d57ea2d565809a4
harshith0712 <kolagatlaharshith@gmail.com>
2020-10-13 21:07:43 +0530

    usecase

--- UserRegistration.sh
+++ UserRegistration.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
-echo "Welcome To User Registration Problem"
+echo "enter the password"
+read password
+passwordPattern="[A-Z]+([a-zA-Z]{7,})"
+
+   if [[ $password =~ $passwordPattern ]]
+   then
+      echo $password
+   else
+      echo "enter correct password"
+   fi
+
+
