3fb46c1ea00a4d0cd917cf7dd06955b828622adb
harshith0712 <kolagatlaharshith@gmail.com>
2020-10-14 11:05:20 +0530

    usecase8

--- UserRegistration.sh
+++ UserRegistration.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-echo "Welcome To User Registration Problem"
+echo "enter the password"
+read password
+	if [[ ${#password} -ge 8 && "$password" == *[A-Z]* && "$password" == *[a-z]* && "$password" == *[0-9]* && "$password" == *[@#%^*]* ]]
+	then
+		echo "valid"
+	else
+		echo "invalid"
+	fi
+
+
