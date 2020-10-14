845fcbf35a4213db37a399fc5d5a1fe5c7a06667
harshith0712 <kolagatlaharshith@gmail.com>
2020-10-14 11:19:58 +0530

    usecase3

--- UserRegistration.sh
+++ UserRegistration.sh
@@ -4,11 +4,11 @@ echo "enter the email"
 read email
 emailPattern="^[a-zA-Z0-9_.+-]*+@[a-zA-Z0-9]*+.[a-zA-Z]{2,4}|.[a-zA-Z]{2}$"
 
-if [[ $email =~ $emailPattern ]]
-then
-echo $email
-else
-echo "enter valid email"
-fi
+	if [[ $email =~ $emailPattern ]]
+	then
+		echo $email
+	else
+		echo "enter valid email"
+	fi
 
 
