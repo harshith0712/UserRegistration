f0f1d0060f0f6910533fb8996415e24dac6ee6b0
harshith0712 <kolagatlaharshith@gmail.com>
2020-10-14 11:21:52 +0530

    usecase8

--- UserRegistration.sh
+++ UserRegistration.sh
@@ -4,9 +4,9 @@ echo "enter the password"
 read password
 	if [[ ${#password} -ge 8 && "$password" == *[A-Z]* && "$password" == *[a-z]* && "$password" == *[0-9]* && "$password" == *[@#%^*]* ]]
 	then
-		echo "valid"
+		echo $password
 	else
-		echo "invalid"
+		echo "enter correct password"
 	fi
 
 
